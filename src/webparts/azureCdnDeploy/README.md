ba9094322ea802318fb8cc6ba1319862e9a35428
Girish Uppal <girish.uppal@gmail.com>
2019-01-17 14:06:58 +1000

    uploaded Azure CDN code

---
+++ src/webparts/azureCdnDeploy/README.md
@@ -0,0 +1,13 @@
+# Deployment Steps
+
+> Bundles the Package
+
+*gulp bundle --ship*
+
+> Deploy contents to CDN
+
+*gulp deploy-azure-storage*
+
+> Generates SPPKG solution for deploying in SharePoint
+
+*gulp package-solution --ship*
---
+++ src/webparts/azureCdnDeploy/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAzureCdnDeployWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AzureCdnDeployWebPartStrings' {
+  const strings: IAzureCdnDeployWebPartStrings;
+  export = strings;
+}
